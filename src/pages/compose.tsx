a019e6092efd41dc3d4e1b401f812b489365fa7e
Juan Vivanco <juansebastianvc@gmail.com>
2022-06-22 08:57:42 -0500

    fix: fix compose

--- src/pages/compose.tsx
+++ src/pages/compose.tsx
@@ -130,7 +130,7 @@ export default function Compose() {
           {({ open }) => (
             <>
               <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-                <span>What is your refund policy?</span>
+                <span>Transaction details</span>
                 <FiChevronUp
                   className={`${
                     open ? 'rotate-180 transform' : ''
@@ -138,8 +138,7 @@ export default function Compose() {
                 />
               </Disclosure.Button>
               <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-                If you're unhappy with your purchase for any reason, email us
-                within 90 days and we'll refund you in full, no questions asked.
+                  Test
               </Disclosure.Panel>
             </>
           )}

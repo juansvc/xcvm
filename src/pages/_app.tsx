9b0a1f8e938b84989c923cafcb7dda013b93638a
Juan Vivanco <juansebastianvc@gmail.com>
2022-06-29 06:34:39 -0500

    chore

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,6 @@ import { AppProps } from 'next/app';
 
 import '@/styles/globals.css';
 
-function getLibrary(provider: any): Web3Provider {
-  const library = new Web3Provider(provider)
-  library.pollingInterval = 12000
-  return library
-}
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
       <Component {...pageProps} />
